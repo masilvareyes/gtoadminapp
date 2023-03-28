e0eedcda7c9e7b99f842caeb59c9a2c90e9cdce3
Marco Silva <ma.silvareyes@gmail.com>
2023-03-27 23:32:42 -0600

    Se modifica el código convirtiendo la plantilla legacy user en código php y separandola en tres secciones: cabecero.php donde se mantiene el codigo de menus y barra superior inicio.php donde se encontrara el cuerpo principal de nuestra aplicación piepagina.php donde se encuentra la barra inferior
    Adicionalmente se añade codigo php para redirigir el sitio por el protocolo seguro (https)

--- vistas/cabecero.php
+++ vistas/cabecero.php
@@ -1,3 +1,14 @@
+<?php 
+
+if ($_SERVER['HTTPS'] != "on") { 
+    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
+    header("Location: $url"); 
+    exit; 
+}
+
+if (strlen(session_id())<1)
+  session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -854,83 +865,3 @@
     </div>
     <!-- /.sidebar -->
   </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Legacy User Menu</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Legacy User Menu</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Title</h3>
-
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
-            </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
-          </div>
-        </div>
-        <div class="card-body">
-          Check the Header part you can find Legacy vesion of style.
-          <br>
-          Start creating your amazing application!
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Footer
-        </div>
-        <!-- /.card-footer-->
-      </div>
-      <!-- /.card -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.2.0
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="../public/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../public/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../public/dist/js/demo.js"></script>
-</body>
-</html>
