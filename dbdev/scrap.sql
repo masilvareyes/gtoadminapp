7fbc3e09703112d35957e0ade9f3faaae4351d90
Marco Silva <ma.silvareyes@gmail.com>
2023-03-29 23:08:56 -0600

    Copiamos nuestro template de la página inicio a departamento para empezar a modificarlo.

--- dbdev/scrap.sql
+++ dbdev/scrap.sql
@@ -10,13 +10,3 @@ CREATE TABLE IF NOT EXISTS departamentos (
   PRIMARY KEY (idDepartamento))
 ENGINE = InnoDB
 
-
-CREATE TABLE IF NOT EXISTS departamentos (
-  idDepartamento INT NOT NULL AUTO_INCREMENT,
-  descripcion VARCHAR(100) NULL,
-  activo TINYINT(1) NOT NULL DEFAULT 1,
-  fechaCreacion DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  fechaActualizacion DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
-  idEmpActualiza INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (idDepartamento))
-ENGINE = InnoDB
